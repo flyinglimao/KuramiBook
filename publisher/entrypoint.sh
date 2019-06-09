80d3bb6125c2eba74717d83869335aebdbb61f32
flyinglimao <flyinglimao@aol.com>
2019-06-09 21:03:16 +0800

    push

--- publisher/entrypoint.sh
+++ publisher/entrypoint.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 # install git
+echo "Install git" && \
 apt-get update && \
 apt-get install -y git && \
 
 # go directory
+echo "CD to workspace" && \
 cd $GITHUB_WORKSPACE && \
 
 # install dependencies
+echo "Install dependencies" && \
 pip install -r requirements.txt && \
+echo "Generate result" && \
 python main.py publisher/tmp.json && \
 cd publisher && \
 mkdir dist && \
 cp static/* dist && \
+echo "Make page" && \
 python make.py && \
 cd dist && \
 
 # config git
+echo "Config git" && \
 git init && \
 git branch gh-pages && \
 git config --global user.email "${COMMIT_EMAIL}" && \
@@ -23,6 +29,7 @@ git config --global user.name "${COMMIT_NAME}" && \
 REPOSITORY_PATH="https://${ACCESS_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" && \
 
 # publish
+echo "Publish page" && \
 git add . && \
 git commit -m "Auto generate $(date +"%F")" && \
 git push $REPOSITORY_PATH gh-pages && \
